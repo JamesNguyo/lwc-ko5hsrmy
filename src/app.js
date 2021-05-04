de928a682b70d76d0fd5d29893d8bbc458daf757
JamesNguyo <james.nguyo@saner.gy>
2021-05-04 12:48:04 +0000

    check calculation of this.recalculatedDuration fees

--- src/app.js
+++ src/app.js
@@ -5,56 +5,36 @@ import { LightningElement, track } from "lwc";
 export default class App extends LightningElement {
   title = "Friendly Loan Company";
   @track principalAmount = 100000;
-  adminFee = 0.00;
-  @track interest = 0.00;
-  processingFee = 0.00;
-  riskFee = 0.00;
+  adminFeeRate = 0.00;
+  @track interestRate = 0.00;
+  processingFeeRate = 0.00;
+  riskFeeRate = 0.00;
+
+  adminFeeRatePreset = 0.00;
+  interestRatePreset = 0.00;
+  processingFeeRatePreset = 0.00;
+  riskFeeRatePreset = 0.00;
+
+  adminFeeRateComputed = 0.00;  
+  interestRateComputed = 0.00;
+  processingFeeRateComputed = 0.00;
+  riskFeeRateComputed = 0.00;
+
   response = "";
   expectedStartDate = new Date();
   expectedEndDate = new Date();
   loanDuration = 0;
-  @track loanMonths = [{"key":String, "mths":[{}]}];
+  @track loanMonthsSchedule = [{"key":String, "mths":[{}]}];
   sumInterest = 0.00;
+  sumFees = 0.00;
+  sumFeesMonthly = 0.00
   monthlyPayment = 0.00;
   totalRepayable = 0.00;
   showFeatures = true;
   monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  selectedMonths = "";
-  // momentInitialized = false;
-
-  // renderedCallback() {
-  //   if (this.momentInitialized) {
-  //       return;
-  //   }
-  //   this.momentInitialized = true;
-  //   loadScript(this, moment)
-  //   .then(() => {
-  //       console.log(window.moment().format());
-  //   })
-  //   .catch(error => {
-  //       console.log(error);
-  //   });
-  // }
-  
-  /**
-   * Getter for the features property
-   */
-  get features() {
-    return [
-      {
-        label: "Learn in the browser.",
-        icon: "utility:edit",
-      },
-      // {
-      //   label: "View changes to code instantly with Live Compilation.",
-      //   icon: "utility:refresh",
-      // },
-      // {
-      //   label: "Style your components with SLDS.",
-      //   icon: "utility:brush",
-      // },
-    ];
-  }
+  recalculatedDuration = 0;
+  earlyPayments = 0.00;
+  recalculatedEndDate = new Date();
 
   get loanProducts() {
     return [
@@ -105,24 +85,50 @@ export default class App extends LightningElement {
 
   onPrincipalSelected(event){
     this.principalAmount = event.target.value;
-    console.log('event.target.value', event.target.value);
+    this.calculateFees();
   }
+
+  onPresetRateChanged(event){
+    console.log("event.target.name",event.target.name);
+    switch (event.target.name){
+      case "adminFeeRate"  :
+        this.adminFeeRate = event.target.value;
+        break;
+      case "interestRate" :
+        this.interestRate = event.target.value;
+        break;
+      case "processingFeeRate"  :
+        this.processingFeeRate = event.target.value;
+        break;
+      case "riskFeeRate"  :
+        this.riskFeeRate = event.target.value;
+        break;
+    }
+    this.calculateFees();
+  }
+
   onLoanProductSelected(event){
     //get loan product details
     //console.log("logging = ", this.loanProducts.find(opt => opt.value === event.detail.value));
     let obj = this.loanProducts.find(opt => opt.value === event.detail.value);
-    this.interest =obj.interest;
-    this.adminFee = obj.admin;
-    this.processingFee = obj.processing;
-    this.riskFee = obj.risk;
+    this.interestRatePreset =obj.interest;
+    this.adminFeeRatePreset = obj.admin;
+    this.processingFeeRatePreset = obj.processing;
+    this.riskFeeRatePreset = obj.risk;
+  
+    this.interestRate =obj.interest;
+    this.adminFeeRate = obj.admin;
+    this.processingFeeRate = obj.processing;
+    this.riskFeeRate = obj.risk;
+    
   }
 
     displayMonths(){
-      this.loanMonths.length = 0;
+      this.loanMonthsSchedule.length = 0;
       
-      let monthlyIntRate = parseFloat((this.interest/100)/this.loanDuration);
-      this.monthlyPayment = parseFloat(this.principalAmount * (monthlyIntRate/(1-Math.pow(1+monthlyIntRate, - this.loanDuration)))).toFixed(2);
-
+      //let monthlyIntRate = parseFloat((/100)/this.loanDuration);
+      this.monthlyPayment = this.calculateMonthlyPayment(this.principalAmount, this.interestRate, this.loanDuration);
+      
       let yr=this.expectedStartDate.getFullYear();
       let i;
       let mthSelected = false;
@@ -151,10 +157,12 @@ export default class App extends LightningElement {
               "runningBalance":0,
               "monthlyPrincipal":this.monthlyPayment,
               "monthlyInterest":0,
-              "plannedPayment":this.monthlyPayment,
+              "plannedPayment": this.monthlyPayment,
+              "monthlyFees" : 0.00,
+              "totalPaymentInclFees": 0.00,
             });
         }
-        this.loanMonths.push({"key":yr, "mths":arrMth,});
+        this.loanMonthsSchedule.push({"key":yr, "mths":arrMth,});
         yr = yr+1;
       }
   }
@@ -169,64 +177,91 @@ export default class App extends LightningElement {
     }
     else 
     {
-      return ((dtEnd - dtStart)/(1000*60*60*24))
+      return ((dtEnd - dtStart)/(1000*60*60*24));
     }
     
   }
 
+  calculateMonthlyPayment(principalAmt, intRate, loanMths ){
+    let mthIntRate = parseFloat((intRate/100)/loanMths);
+    return parseFloat(principalAmt * parseFloat((mthIntRate/(1-Math.pow(1+mthIntRate, - loanMths))))).toFixed(2);
+    //parseFloat(this.principalAmount * (monthlyIntRate/(1-Math.pow(1+monthlyIntRate, - this.loanDuration)))).toFixed(2);
+  }
+
   amortiseSchedule() {
+    this.calculateFees();
     let previousBal = this.principalAmount;
-    let monthlyIntRate = parseFloat((this.interest/100)/this.loanDuration);
+    let monthlyIntRate = parseFloat((this.interestRate/100)/this.loanDuration);
     let interestAmt = 0.00;
     this.monthlyPayment = parseFloat(this.principalAmount * (monthlyIntRate/(1-Math.pow(1+monthlyIntRate, - this.loanDuration)))).toFixed(2);
+    //this.monthlyPayment = parseFloat(this.monthlyPayment) + parseFloat(this.sumFeesMonthly);
+    //this.monthlyPayment = parseFloat(this.calculateMonthlyPayment(this.principalAmount, monthlyIntRate, this.loanDuration)).toFixed(2);
+    
     this.totalRepayable = parseFloat(this.monthlyPayment * this.loanDuration).toFixed(2);
-    this.sumInterest = parseFloat(this.totalRepayable - this.principalAmount).toFixed(2);
-    this.selectedMonths = "";
-    this.loanMonths.map((i) => 
+    //this.sumInterest = parseFloat(this.totalRepayable - this.principalAmount).toFixed(2);
+    this.recalculatedDuration = 0;
+    this.sumInterest = 0.00;
+    
+    this.loanMonthsSchedule.map((i) => 
     {
-      this.sumInterest = 0.00;
       (i.mths.map((e)=> {
-        // if(e.key === event.target.name){
-        //   e.selected = event.target.checked;
-        // }
-        if(e.adjPaymentDate >= this.expectedStartDate && e.adjPaymentDate <= this.expectedEndDate){
-          e.plannedPayment = e.plannedPayment?e.plannedPayment: parseFloat(this.monthlyPayment).toFixed(2);
-          interestAmt = parseFloat(previousBal * monthlyIntRate);
-          e.monthlyInterest = parseFloat(interestAmt).toFixed(2);
-          e.monthlyPrincipal = parseFloat(e.plannedPayment - interestAmt).toFixed(2);
-          previousBal = previousBal - e.monthlyPrincipal;
-          this.sumInterest = parseFloat(this.sumInterest +  e.monthlyInterest ).toFixed(2)
+        e.selected = false;
+        if(e.adjPaymentDate >= this.expectedStartDate ){
+          e.runningBalance = previousBal >= 0 ? parseFloat(previousBal).toFixed(2) :0;
+          if(e.runningBalance > 0){
+            e.monthlyPayment = parseFloat(this.monthlyPayment).toFixed(2);
+            e.monthlyFees = parseFloat((this.sumFees)/this.loanDuration).toFixed(2) ;
+            e.plannedPayment = parseFloat(e.plannedPayment);// + parseFloat(e.monthlyFees);
+            //parseFloat(this.monthlyPayment).toFixed(2);
+            //e.plannedPayment = e.plannedPayment?e.plannedPayment: parseFloat(this.monthlyPayment).toFixed(2)
+            interestAmt = parseFloat(previousBal * monthlyIntRate);
+            e.monthlyInterest = parseFloat(interestAmt).toFixed(2);
+            e.monthlyPrincipal = parseFloat(e.plannedPayment - interestAmt).toFixed(2);
+            previousBal = previousBal - e.monthlyPrincipal;
+            this.sumInterest = parseFloat(parseFloat(this.sumInterest) + parseFloat(e.monthlyInterest)).toFixed(2) ;
+            e.totalPaymentInclFees = e.plannedPayment > 0 ? parseFloat(parseFloat(e.plannedPayment) + parseFloat(e.monthlyFees)).toFixed(2):0.00;
+            this.recalculatedDuration = this.recalculatedDuration +  e.plannedPayment > 0 ?1:0;
+            e.selected = e.plannedPayment > 0 ? true : false;
+          }
+          else 
+          {
+            e.plannedPayment = 0.00;
+            e.monthlyInterest = 0.00;
+            e.monthlyPrincipal = 0.00;
+            e.monthlyPayment = 0.00;
+            e.totalPaymentInclFees = 0.00;
+          }
         }
+        else 
+          {
+            e.plannedPayment = 0.00;
+            e.monthlyInterest = 0.00;
+            e.monthlyPrincipal = 0.00;
+            e.monthlyPayment = 0.00;
+            e.totalPaymentInclFees = 0.00;
+          }
         })
       )
     })
   }
 
    onMonthSelected(event){
-    console.log("checked = ", event.target.checked, '***', event.target.name);
-    //console.log(this.loanMonths);
-    //Monthly Payment Formula used = r = (1 + i)^(1/n) - 1
-
-    //console.log("in here", previousBal);
-    //this.selectedMonths = this.selectedMonths.concat( e.key.toString(),", ") ;
+    
    }
 
    onPlannedAmountChanged(event){
-    console.log("checked = ", event.target.value, '***', event.target.name);
-    this.loanMonths.map((i) => 
+    this.loanMonthsSchedule.map((i) => 
     {
       (i.mths.map((e)=> {
+        
         if(e.key === event.target.name){
-          e.plannedPayment = event.target.value;
-          console.log("hapa",e.plannedPayment);
-          
+          e.selected = event.target.value > 0 ? true : false;
+          e.plannedPayment = event.target.value;        
         }
         })
       )
     })
-    //this.displayMonths();
     this.amortiseSchedule();
-    //this.displayMonths();
    }
 
   onLoanDateChanged(event){
@@ -237,12 +272,22 @@ export default class App extends LightningElement {
       this.expectedEndDate = new Date(event.detail.value);
     }
     this.loanDuration = this.expectedEndDate - this.expectedStartDate;
-    //console.log(event.target.name);
     this.loanDuration = this.expectedEndDate.getMonth()+ (12*(this.expectedEndDate.getFullYear()-this.expectedStartDate.getFullYear())) - this.expectedStartDate.getMonth();
-
+    
+    this.calculateFees();
+    
     this.displayMonths();
     this.amortiseSchedule();
     //console.log("this this.expectedStartDate = ",this.expectedStartDate.getMonth(), '***',this.expectedStartDate.getFullYear());
+  }
+
+  calculateFees(){
+    this.processingFeeRateComputed = parseFloat(this.processingFeeRate)/100 * parseFloat(this.principalAmount);
+    this.adminFeeRateComputed = ((parseFloat(this.adminFeeRate)/100)* parseFloat(this.principalAmount));
+    this.riskFeeRateComputed = ((parseFloat(this.riskFeeRate)/100)* parseFloat(this.principalAmount));
+    this.interestRateComputed = this.calculateMonthlyPayment(this.principalAmount, this.interestRate, this.loanDuration) * this.loanDuration - this.principalAmount; 
+    this.sumFees = parseFloat(this.processingFeeRateComputed + this.adminFeeRateComputed + this.riskFeeRateComputed).toFixed(2);
+    this.sumFeesMonthly = parseFloat(this.sumFees/this.loanDuration).toFixed(2);
   }
 
  }
